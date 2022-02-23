6c5634fe063d4f8b2e1351e53a1b3bc4ae6955e7
Ezy93 <giordani001@gmail.com>
2022-02-23 16:41:29 +0100

    inzio costruzione funzionalita pagina

---
+++ js/script.js
@@ -0,0 +1,8 @@
+let userName = document.getElementById("userNameInput").innerHTML;
+console.log(userName);
+
+let tripLength = document.getElementById("tripLengthInput").innerHTML;
+console.log(tripLength);
+
+let userAge = document.getElementById("userAgeInput").value;
+console.log(userAge);
