901e87fd1d9ef5bc49a1dc4b8d88ecc477cdb6d8
Ezy93 <giordani001@gmail.com>
2022-02-23 17:05:27 +0100

    inizializzazione di variabili e constanti in js

--- js/script.js
+++ js/script.js
@@ -6,3 +6,17 @@ console.log(tripLength);
 
 let userAge = document.getElementById("userAgeInput").value;
 console.log(userAge);
+
+const btnGenerator = document.getElementById(ticketGenerator);
+
+const pricePerKm = 0.27;
+
+const fullPriceTicket = tripLength * 0.27;
+
+let teenAgeTicket = fullPriceTicket * 0.17;
+
+let oldAgeTicket = fullPriceTicket * 0.33;
+
+
+
+
