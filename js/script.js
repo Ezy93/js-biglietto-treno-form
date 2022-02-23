92d89d65630897dd20a9d6d8973975c925335800
Ezy93 <giordani001@gmail.com>
2022-02-23 18:57:46 +0100

    non riesco a risolvere il value dilemma

--- js/script.js
+++ js/script.js
@@ -7,6 +7,8 @@ console.log(tripLength);
 let userAge = document.getElementById("userAgeInput").value;
 console.log(userAge);
 
+
+
 const btnGenerator = document.getElementById("ticketGenerator");
 
 
@@ -19,10 +21,32 @@ let teenAgeTicket = fullPriceTicket * 0.17;
 let oldAgeTicket = fullPriceTicket * 0.33;
 
 
+
 btnGenerator.addEventListener("click", function() {
-    
+
     userName = document.getElementById("userNameInput").value;
     document.getElementById("passengerName").innerHTML = userName;
+
+    if (userAge = "minorenne"){
+
+    document.getElementById("finalPrice").innerHTML = teenAgeTicket.toFixed(2);
+    document.getElementById("offering").innerHTML = "tariffa ridotta per minorenni";
+
+    }else if(userAge = "over65"){
+
+        document.getElementById("finalPrice").innerHTML = oldAgeTicket.toFixed(2);
+        document.getElementById("offering").innerHTML = "tariffa ridotta per anziani";
+
+    }else{
+        
+        document.getElementById("finalPrice").innerHTML = fullPriceTicket.toFixed(2);
+        document.getElementById("offering").innerHTML = "tariffa ordinaria";
+
+    };
+
+
+
+
 
     let dnoneRemove = document.getElementById("hidden");
     dnoneRemove.classList.remove("d-none");
