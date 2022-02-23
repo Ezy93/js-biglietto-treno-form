d55388cbe1b3618dc890e8a0ce05ef61727199ed
Ezy93 <giordani001@gmail.com>
2022-02-23 17:41:19 +0100

    aggiunto event listener in js

--- js/script.js
+++ js/script.js
@@ -1,13 +1,14 @@
-let userName = document.getElementById("userNameInput").innerHTML;
+let userName = document.getElementById("userNameInput").value;
 console.log(userName);
 
-let tripLength = document.getElementById("tripLengthInput").innerHTML;
+let tripLength = document.getElementById("tripLengthInput").value;
 console.log(tripLength);
 
 let userAge = document.getElementById("userAgeInput").value;
 console.log(userAge);
 
-const btnGenerator = document.getElementById(ticketGenerator);
+const btnGenerator = document.getElementById("ticketGenerator");
+
 
 const pricePerKm = 0.27;
 
@@ -18,5 +19,11 @@ let teenAgeTicket = fullPriceTicket * 0.17;
 let oldAgeTicket = fullPriceTicket * 0.33;
 
 
-
+btnGenerator.addEventListener("click", function() {
+    
+    document.getElementById("passengerName").innerHTML = userName;
+    
+    let dnoneRemove = document.getElementById("hidden");
+    dnoneRemove.classList.remove("d-none");
+});
 
